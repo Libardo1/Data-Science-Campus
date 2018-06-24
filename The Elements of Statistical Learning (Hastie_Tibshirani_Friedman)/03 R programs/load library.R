fc6f92eb422570adfbbb66bf3301993e88a9faf3
awash-analytics <awash.analytics@gmail.com>
2018-06-24 14:11:19 +0200

    The_EoSL - upcoming
    The_EoSL - upcoming

---
+++ The Elements of Statistical Learning (Hastie_Tibshirani_Friedman)/03 R programs/load library.R
@@ -0,0 +1,6 @@
+
+library(tidyverse)
+
+library(lasso2)     ## R Package to solve regression problems while imposing an L1 constraint on the parameters.
+library(glmnet)     ## Lasso and Elastic-Net Regularized Generalized Linear Models
+
---
+++ The Elements of Statistical Learning (Hastie_Tibshirani_Friedman)/Chapter 3 - The_EoSL.R
@@ -0,0 +1,6 @@
+
+## load library
+source("./03 R programs/load library.R")
+
+## Load dataset
+data("Prostate", package = "lasso2")
