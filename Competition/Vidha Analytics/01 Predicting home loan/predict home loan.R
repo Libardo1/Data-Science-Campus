0b773c4953659bb2e430c45485b9e3a9c2537933
awash-analytics <awash.analytics@gmail.com>
2018-07-02 22:04:00 +0200

    predicting home loan - upcoming
    predicting home loan - upcoming

--- Competition/Vidha Analytics/01 Predicting home loan/predict home loan.R
+++ Competition/Vidha Analytics/01 Predicting home loan/predict home loan.R
@@ -3,6 +3,7 @@
 ## load library
 source("./03 R programs/load library.R")
 source("./03 R programs/plot_association_eda.R")
+source("./03 R programs/plot_histogram_eda.R")
 
 ## load dataset
 train_raw <- readr::read_csv(file = file.choose(), col_names = T)
@@ -53,6 +54,29 @@ train$outlier_ApplicantIncome <- outliers::scores(x = train$ApplicantIncome,
                                                   type = "chisq", 
                                                   prob = 0.9)
 
+
+## Histogram
+# ggplot(data = train, aes(ApplicantIncome)) + 
+#   geom_histogram(binwidth = 35)
+
+plot_histogram_eda(dsin = train, id_var = "Loan_ID")
+
+## ---------------------------------------------------
+## -- two variables are excluded from histogram
+## ---------------------------------------------------
+## obtain Credit_History
+## -- values: 1, 0, NA
+## --- T2DO: convert Credit_History as factor variable
+train %>% 
+  dplyr::pull(Credit_History) %>% 
+  unique()
+
+## obtain Loan_Amount_Term
+## -- values: 360 120 240  NA 180  60 300 480  36  84  12
+## --- T2DO: convert Loan_Amount_Term as factor variable
+train %>% 
+  dplyr::pull(Loan_Amount_Term) %>% 
+  unique()
 
 
 
