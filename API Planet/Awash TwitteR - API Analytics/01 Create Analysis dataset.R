8756bdc2c53b234ce656591cab4292988bc896d2
awash-analytics <awash.analytics@gmail.com>
2018-07-08 08:47:43 +0200

    Awash Twitter analytics - upcoming
    Awash Twitter analytics - upcoming

--- API Planet/Awash TwitteR - API Analytics/01 Create Analysis dataset.R
+++ API Planet/Awash TwitteR - API Analytics/01 Create Analysis dataset.R
@@ -9,31 +9,22 @@ source("./03 R programs/load library.R")
 tweet_raw <- readr::read_csv(file = file.choose(), col_names = T)
 
 ## Extract websites
-# test <- tweet_raw %>% 
+# test <- tweet_raw %>%
 #   dplyr::filter(row_number() == c(seq(1,10)))
   
 tweet_prep <- tweet_raw %>%
-# tweet_prep <- test %>% 
-  dplyr::group_by(text) %>% 
-  dplyr::mutate(link_raw = paste(as.character(unlist(stringr::str_extract_all(string = text,
-                                                                              pattern = "(\\w+)?(https)\\:\\//\\w+\\.\\w+((\\/\\w+)?)"))),
-                                 collapse = " ")) %>%
-  dplyr::mutate(link = paste(unique(as.character(unlist(stringr::str_split(string = link_raw, 
-                                                                           pattern = "\\s+")))), 
-                             collapse = " ")) %>%      ## remove duplicates
-  dplyr::mutate(user_network_raw = paste(as.character(unlist(stringr::str_extract_all(string = text,
-                                                                                      pattern = "\\@\\w+"))),
-                                         collapse = " ")) %>%
-  dplyr::mutate(user_network = paste(unique(as.character(unlist(stringr::str_split(string = user_network_raw, 
-                                                                                   pattern = "\\s+")))), 
-                                     collapse = " ")) %>%      ## remove duplicates
-  dplyr::mutate(user_hashtag_raw = paste(as.character(unlist(stringr::str_extract_all(string = text,
-                                                                                      pattern = "\\#\\w+"))),
-                                         collapse = " ")) %>%
-  dplyr::mutate(user_hashtag = paste(unique(as.character(unlist(stringr::str_split(string = user_hashtag_raw, 
-                                                                                   pattern = "\\s+")))), 
-                                     collapse = " ")) %>%       ## remove duplicates
-  dplyr::select(-dplyr::contains("_raw")) 
+# tweet_prep <- test %>%
+  dplyr::group_by(screenName, created) %>% 
+  dplyr::mutate(link = paste(as.character(unlist(stringr::str_extract_all(string = text,
+                                                                          pattern = "(\\w+)?(https)\\:\\//\\w+\\.\\w+((\\/\\w+)?)"))),
+                             collapse = " ")) %>%
+  dplyr::mutate(user_network = paste(as.character(unlist(stringr::str_extract_all(string = text,
+                                                                                  pattern = "\\@\\w+"))),
+                                     collapse = " ")) %>%
+  dplyr::mutate(user_hashtag = paste(as.character(unlist(stringr::str_extract_all(string = text,
+                                                                                  pattern = "\\#\\w+"))),
+                                     collapse = " ")) %>%
+  dplyr::select(-dplyr::contains("_raw"))
 
 
 ## Save analysis dataset
